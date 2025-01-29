c2815e722aa38e01852238913062b1c47dbb4fea
valerio minorenti <v.minorenti.02@gmail.com>
2025-01-29 16:23:37 +0100

    Algoritmo con le funzione per manipolare i dati grezzi

--- Algoritmo_manipolazione_dati.py
+++ Algoritmo_manipolazione_dati.py
@@ -1,93 +1,300 @@
-import pandas as pd
+import pandas as pd # type: ignore
+import os
 
-#  Percorso assoluto per arrivare al file Excel
-file_path = "C:/Users/Standard/Desktop/0_DATAFRAME_EXCEL.xlsx"  #  il percorso  file
-df = pd.read_excel(file_path)
 
-#Passo 1: eliminare le colonne con campi inutili
-# Elimina le colonne A, B, H, si scrivono semplicemnte gli indici delle colonne e ovviamente la riga 
-df = df.drop(df.columns[[0, 1, 7]], axis=1)
-#messaggio che dice che sono state eliminate le colonne
-print("Colonne A, B e H eliminate,")
+#PASSO 0
+def salva_percorso(input_path, output_path):
+    """
+   salva i percorsi e poi li dice 
 
-#Passo 2: si ordinano tutti gli id, dal più basso al più alto. 
-# prendo i numeri della prima colonna  e li ordino dal più basso al più alto
+    args:
+        input_path (str): oil percorso del file di input
+        output_path (str): percorso dell'output
 
-df = df.sort_values(by=df.columns[0]) #sort in automatico ordina in modo crescente
-#messaggio per dire che sono state ordinati gli ID
-print("Colonna ordinata in modo crescente")
+    return:
+        il percorso input e output
+    """
+    print(f"Percorso del file di input: {input_path}")
+    print(f"Percorso del file di output: {output_path}")
+    return input_path, output_path
 
-#PASSO 3: eliminare i duplicati, confronto tutti ivalori delle righe
 
-# serve per eliminare le righe con duplicati dalla colonna da B a J, uso gli indici di colonna da  1 a 9
+#Passo 0.5
+def carica_file(input_path):
+    """
+    il file tabulare lo si carica come df
 
-df = df.drop_duplicates(subset=df.columns[1:10])
-#messaggio per dire che si è eliminato i duplicati
-print("Righe duplicate dalla colonna B alla J eliminate")
+    Args:
+        input_path (str): percorso dell'input
 
-#PASSO 4: convertire tutti i valori nele celle in valori numerici, perché alcune non ce l'hanno e poi uniformo in modo da non avere decimali
-# Converto le colonne da A a J in numeri, però devo convertire le , in . altrimenti python mi da ValueError.
-for col in df.columns[:10]:  #le colonne vanno da 1 a 10.
-    # Sostituisco le virgole con i punti
-    df[col] = df[col].replace({',': '.'}, regex=True)
+    Returns:
+        pd.DataFrame: df con i dati
+    """
+    ext = os.path.splitext(input_path)[1].lower()  # estrae l'estensione del file
     
-    # Converto in numerico
-    df[col] = pd.to_numeric(df[col], errors='raise')
+    try:
+        if ext == ".csv":
+            df = pd.read_csv(input_path)
+        elif ext == ".xlsx":
+            df = pd.read_excel(input_path)
+        elif ext == ".tsv":
+            df = pd.read_csv(input_path, sep='\t')
+        else:
+            print(f"Errore: l'estensione {ext} non è supportata, aggiungila.")
+            return None
+        print("file caricato correttamente")
+        return df
+    except FileNotFoundError:
+        print(f"Errore! {input_file} non esiste o è errato il path")
+        return None
+    except Exception as e:
+        print(f"errore nel caricamento {e}")
+        return None
+    
+#PAsso 1
+def elimina_colonne(df, colonne_da_eliminare):
+    """
+    specifichi le colonne nel dataframe  e vengono eliminate, solo nomi di colonne non indici
 
-#PASSO 5: Alcuni numeri sono errati, infatti ci sono 999 oppure degli 0, i numeri >10 sono sbagliati e vanno convertiti in 9 e gli 0 in 1.
-# Applica le modifiche per le colonne da B a J (colonne 1 a 9)
-for col in df.columns[1:10]:  # Colonne da B a J, non cpnverto la A perchè ovviamente gli ID sono maggiori di 10.
-    # Sostituisci con 10 se il numero è maggiore di 10, altrimenti 0 se è vuoto o NaN
-    df[col] = df[col].apply(lambda x: 10 if x > 10 else (1 if pd.isna(x) else x))
+    args:
+        df (pd.DataFrame): il df da cui si eliminano le colonne.
+        colonne_da_eliminare (list): la lista dove ci sono le colonne da eliminare.
+
+    Returns:
+        pd.DataFrame: lo stesso df ma senza le colonne che sono state scritte.
+    """
+    df = df.drop(columns=colonne_da_eliminare, errors='ignore')
+    print(f"le colonne eliminate sono: {colonne_da_eliminare}")
+    return df
 
 
-#messaggio per dire che si è convertito i valori
-print("Tutti gli 0 sono 1 e i >10 sono 10")
+#Passo 2
+def ordina_colonna(df, colonna_id):
+    """
+    ordina una colonna specifica in ordine crescente
 
-#Passo 6:  la colonna CLass Type va corretta perchè ci possono essere solo 2 valori che sono 2 e 4. quindi tutti gli 1 vanno convertiti in 2
- 
-# se trovo un 1 lo sostituisco con 2,  le colonne le ho chiamate con nome invece di indice
+    Args:
+        df (pd.DataFrame): il df da ordinare.
+        colonna_id (str): Il nome della colonna da ordinare.
 
-df['classtype_v1'] = df['classtype_v1'].replace(1, 2)
+    Returns:
+        pd.DataFrame: df con la colonna scritta però ordinata in modo crescente
+    """
+    if colonna_id in df.columns:
+        df = df.sort_values(by=colonna_id)
+        print(f"la colonna {colonna_id} è stata ordinata in modo crescente")
+    else:
+        print(f"non esiste alcuna colonna intitolata {colonna_id}, nessuna colonna è stat ordinata")
+    return df
 
-#messaggio per dire che sono stati cambiati gli 1 con i 2
-print(" gli 1 nella colonna H sono stati sostituiti con 2")
 
-#PAsso 7: vanno divise visivamente e logcamnte le features e le classi, quindi si  "scambiano" le colonne.
+#PAsso 3
+def elimina_duplicati_su_colonna(df, colonna):
+    """
+    elimina i duplicati(righe) prendnedo come riferimento una colonna
+    elimina anche le righe che per quella colonna hanno valori vuoti o NaN
+    
 
-# Scambia i valori delle colonne H e J
-df['classtype_v1'], df['bareNucleix_wrong'] = df['bareNucleix_wrong'], df['classtype_v1']
+    Args:
+        df (pd.DataFrame): il df da ordinare.
+        colonna (str): nome colonna su cui basarsi.
 
-# Scambia anche i nomi delle colonne
-df = df.rename(columns={'classtype_v1': 'bareNucleix_wrong', 'bareNucleix_wrong': 'classtype_v1'})
-#messaggio
-print(" colonne H e J scambiate")
-#PASSO 8: ultimo controllo di eliminazione dei duplicati, però adesso si controllano gli ID e non i valori.
-# Nome della colonna da controllare per duplicati
-colonna_da_copiare = 'Sample code number'
+    Returns:
+        pd.DataFrame: il df ripulito dai valori duplicati  o nulli
+    """
+    if colonna in df.columns:
+        # Elimina le righe con valori nulli o vuoti nella colonna selezionata
+        df = df.dropna(subset=[colonna])  # Elimina righe con NaN nella colonna
+        df = df[df[colonna] != ""]  # Elimina righe con stringa vuota nella colonna
+        
+        # viene mantenuta solo la prima colonna "duplicata"
+        df = df.drop_duplicates(subset=[colonna])
+        
+        print(f"eliminate le righe associate a tutti i duplciati e i valori nulli della colonna {colonna}.")
+    else:
+        print(f"Attenzione! Non esiste alcuna colonna chiamata {colonna}.")
 
-# Elimina le righe con valori ugualia
-df_senza_duplicati = df.drop_duplicates(subset=colonna_da_copiare, keep='first') #drop_duplicates mi controlla se ho duplicati e li droppa, è una funzione di base di pandas
-#Sono finiti i processi di manipolazione quindi si può salvare un nuovo file
-"""
-#Si può fare un controllo finale in cui si vede se ci sono degli ID duplicati.
-# Nome della colonna 
-colonna_da_copiare = 'Sample code number'
+    return df
 
-# Copia i valori della colonna in una lista
-valori_colonna_lista = df[colonna_da_copiare].tolist()
 
-# Trova i valori duplicati
-conta_valori = Counter(valori_colonna_lista)
-duplicati = [item for item, count in conta_valori.items() if count > 1]
+#PASSO 4
+def converti_a_numerico(df):
+    """
+    tutte le celle di tutte le colonna diventano valori accettabili e numeri interi
 
-# Stampa i duplicati trovati
-if duplicati:
-    print("Valori duplicati trovati:", duplicati)
-else:
-    print("Nessun valore duplicato trovato.")
-"""
+    Args:
+        df (pd.DataFrame): df da manipolare
 
-print("Righe duplicate eliminate e nuovo file salvato!")
-# Salva il file finale dell'excel
-df.to_excel("Tabella_Dati_FIA.xlsx", index=False)
+    Returns:
+        pd.DataFrame: df con le colonne di soli numeri.
+    """
+    for col in df.columns:
+        # Sostituisco le virgole con i puntile virgole diventano punti
+        df[col] = df[col].replace({',': '.'}, regex=True)
+        
+        # converto tutto in numeri
+        df[col] = pd.to_numeric(df[col], errors='coerce')  # Usa 'coerce' per evitare errori di conversione
+
+        # serve per arrotondare 
+        df[col] = df[col].round(0).astype("Int64")  # Int64 gestisce anche i valori NaN, che non dovrebbero esserci
+        
+    print("Tutte le colonne ora sono formate da valori numerici")
+    return df
+
+
+
+#PASSO 5
+def correggi_valori(df):
+    """
+   Corregge(sostituisce) i valori numerici nelle colonne che non sono quella di ID
+   in questo caso:
+       1- se il numero è 0 o NaN diventa 1
+       2- se è >10 ma divisibile per 10 senza resto diventa quel numero
+       3- se è >10 ma non divisibile intero per 10 diventa 9
+   
+    Args:
+        df (pd.DataFrame): Il df da correggere.
+
+    Returns:
+        pd.DataFrame: Il df con i valori sostituii
+    """
+    for col in df.columns[1:]:  # parte da  dopo la prima colonna 
+        df[col] = df[col].apply(lambda x: (
+            1 if pd.isna(x) or x == 0 else  # NaN o 0 = 1
+            int(x // 10) if x > 10 and x % 10 == 0 else  # se >10 e %10=0 diventa il numero
+            9 if x > 10 else  # se >10 e non 2, duventa 9
+            int(x)  # altri casi son o tutti uguali
+        ))
+
+    return df
+
+
+#Passo 6
+def correggi_class_type(df, colonne_da_correggere):
+    """
+    corregge tutti i valori 1 in 2, serve per la class type.
+
+    Args:
+        df (pd.DataFrame): Il df da correggere.
+        colonne_da_correggere (list): colonna da correggere.
+
+    Returns:
+        pd.DataFrame: Il df con le colonne corrette.
+    """
+    for col in colonne_da_correggere:
+        if col in df.columns:
+            df[col] = df[col].replace(1, 2)  # gli 1 diventano 2 
+            print(f"Gli 1 nella colonna '{col}' sono stati sostituiti con 2.")
+        else:
+            print(f"Attenzione! La colonna '{col}' non esiste .")
+
+    return df
+
+#Passo 7
+def scambia_colonne(df, colonna1, colonna2):
+    """
+    "Scambia" i valori e i nomi di due colonne.
+
+    Args:
+        df (pd.DataFrame): Il df su cui effettuare lo scambio.
+        colonna1 (str):   prima colonna.
+        colonna2 (str):  seconda colonna.
+
+    Returns:
+        pd.DataFrame: Il df con le colonne scambiate.
+    """
+    if colonna1 in df.columns and colonna2 in df.columns:
+        # imverte i valori delle colonne
+        df[colonna1], df[colonna2] = df[colonna2], df[colonna1]
+
+        # i nomi si scambiano
+        df = df.rename(columns={colonna1: colonna2, colonna2: colonna1})
+
+        print(f"{colonna1} e {colonna2} sono state invertite.")
+    else:
+        print(f"Attenzione! {colonna1} o {colonna2} non esistono.")
+
+    return df
+
+#PAsso ultimo
+def salva_file(df, output_path):
+    """
+    salva il df nel formato tabulare desiderato (supportato)
+
+    Args:
+        df (pd.DataFrame): Il df da salvare.
+        output_path (str): path del file di output.
+
+    Returns:
+        None
+    """
+    ext = os.path.splitext(output_path)[1].lower()  # estrae l'estensione
+    
+    try:
+        if ext == ".csv":
+            df.to_csv(output_path, index=False)
+        elif ext == ".xlsx":
+            df.to_excel(output_path, index=False, engine='openpyxl')  # engine
+        elif ext == ".tsv":
+            df.to_csv(output_path, sep='\t', index=False)
+        else:
+            print(f"attenzione! l'estensione {ext} non è supportata, aggiungila")
+            return
+        print(f"Fìile salvato con successo in: {output_path}")
+    except Exception as e:
+        print(f"Errore durante il salvataggio del file: {e}")
+        
+        
+        #####################
+#PASSO 0: l'utente deve avere due file tabulari e deve specificare i percorsi
+ ##Da qui si chiamano le funzione e l'utente deve scrivere
+if __name__ == "__main__":
+    # scrivere i percorsi qui, se il  file tabulare è diverso da csv,tsv o xlsx va aggiunto nella funzione salvafile!
+    input_file = "C:/Users/Standard/Desktop/version_1.csv"  
+    output_file = "C:/Users/Standard/Desktop/Dati_Progetto_Gruppo4.csv" 
+
+   #  sono salvati e vengono anche ridati
+    input_file, output_file = salva_percorso(input_file, output_file)
+
+    # Carica il file nel df
+    df = carica_file(input_file)
+
+   # PASSO 1: eliminare le colonne non utili ai fini del progetto
+   # l'utente deve scrivere i nomi delle colonne. vanno rispettati i caretteri e più colonne devono essere separate da virgole(scrivere nell'input?) 
+    colonne_input = input("Inserisci i nomi delle colonne da eliminare, separati da una virgola: ") 
+    colonne_da_eliminare = [col.strip() for col in colonne_input.split(",")] 
+    
+   #si usa la funzione per elimianre 
+    df = elimina_colonne(df, colonne_da_eliminare) 
+    
+   #PASSO 2: ordina la colonna ID
+    colonna_da_ordinare = input("Inserisci il nome della colonna da ordinare: ").strip()
+    df = ordina_colonna(df, colonna_da_ordinare)
+    
+   # PASSO 3: eliminare le righe duplicate in base a una colonna scelta dall'utente
+    colonna_scelta = input("Inserisci il nome della colonna su cui eliminare i duplicati e i valori nulli: ")
+    df = elimina_duplicati_su_colonna(df, colonna_scelta)
+    ###################
+    # PASSO 3: eliminare le righe completamente duplicate
+   # df = df.drop_duplicates()  # Confronta tutte le colonne e rimuove i duplicati
+    #print("Righe duplicate eliminate considerando tutte le colonne.")
+    ##################
+    
+    # PASSO 4: convertire tutte le colonne in numeri interi
+    df = converti_a_numerico(df)  
+    
+    # PASSO 5: correggere i numeri errati
+    df = correggi_valori(df)
+    
+    # PASSO 6: correggere le colonne scelte dall'utente (1 diventa2)
+    colonne_class_type = input("Inserisci i nomi delle colonne in cui convertire gli 1 in 2, separati da una virgola: ")
+    colonne_da_correggere = [col.strip() for col in colonne_class_type.split(",")]
+    
+    df = correggi_class_type(df, colonne_da_correggere)
+  # PASSO 7: scambiare due colonne scelte dall'utente
+    colonna1 = input("Inserisci il nome della prima colonna da scambiare: ").strip()
+    colonna2 = input("Inserisci il nome della seconda colonna da scambiare: ").strip()
+    df = scambia_colonne(df, colonna1, colonna2)
+    
+   # df salvato
+    salva_file(df, output_file)
